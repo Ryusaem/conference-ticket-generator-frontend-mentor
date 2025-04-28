769c4b0970898225e0bb9d993c401273f944a0aa
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2025-04-28 16:39:03 +0200

    Adding Form functionality to be dynamic v0.1

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@ const el = document.querySelector(".coding-conf");
 const hero = el.querySelector(".coding-conf__hero");
 const form = el.querySelector(".coding-conf__form");
 const btn = el.querySelector(".coding-conf__btn");
-
 const ticket = el.querySelector(".coding-conf__ticket");
 
+// Event Listener
+// btn.addEventListener("click", something);
+
+form.addEventListener("submit", copy);
+
+function copy(e) {
+  e.preventDefault();
+  const fullName = form.fullName.value.trim();
+  const email = form.email.value.trim();
+  const githubUserName = form.githubUsername.value.trim();
+  const fileInput = form.querySelector('input[type="file"]');
+  const file = fileInput.files[0];
+
+  console.log({ file });
+}
 function something() {
   hero.style.display = "none";
   form.style.display = "none";
   ticket.style.display = "grid";
 }
-
-btn.addEventListener("click", something);
